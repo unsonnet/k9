a3d866a4294ec4b17fdc0169a99fd24286de9aec
Rodriguez, Santiago <santiago.rodriguez@gtri.gatech.edu>
2025-10-02 16:58:22 -0400

    Prepare for github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/k9',
+  assetPrefix: '/k9',
   images: {
     unoptimized: true
   }
